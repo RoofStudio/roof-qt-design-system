79f3f3c8d93705f1c1083413c6b0cf2352da1aed
C.Hallan <carloshallan@gmail.com>
2023-04-28 13:42:32 -0300

    Release Low Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qt-design-system",
-    version='0.4.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         "pyside6",
