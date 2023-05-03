35eb0fbb6688dbf8f5bf2bf42d891ef1174288e6
C.Hallan <carloshallan@gmail.com>
2023-05-02 22:39:38 -0300

    remove build styles

--- RoofDesignSystem/styles/__init__.py
+++ RoofDesignSystem/styles/__init__.py
@@ -4,10 +4,10 @@ import sys
 
 def get_stylesheet():
     # Determine the base directory
-    if getattr(sys, "frozen", False):
-        base_dir = os.path.join(sys._MEIPASS, "app", "styles")
-    else:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
+    # if getattr(sys, "frozen", False):
+    #     base_dir = os.path.join(sys._MEIPASS, "app", "styles")
+    # else:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
 
     folder = os.path.join(base_dir)
     styles = filter(lambda filename: filename.endswith("css"), os.listdir(folder))
